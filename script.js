a39edd5727dbbac7abb568bb1913efb67a7fd470
Balbesukys <lekha.arkhipov414@gmail.com>
2024-10-22 22:34:19 +0300

    Подправил дз и js

---
+++ script.js
@@ -0,0 +1,68 @@
+
+//Задание1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//Задание2
+const dateIphone = 2006;
+console.log(dateIphone);
+
+
+
+//Задание3
+const nameCreatorJS = "Брендан Эйх"
+console.log(nameCreatorJS);
+
+//Задание4
+
+let b = 10;
+let c = 2;
+
+alert(b + c);
+alert(b - c);
+alert(b * c);
+alert(b / c);
+
+//Задание5
+
+let result = 2 ** 5;
+alert(result);
+
+//Задание6
+
+let d = 9;
+let f = 2;
+let rest = d % f;
+alert(rest);
+
+//Задание7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+//Задание 8
+
+let age = prompt('Сколько вам лет');
+alert(age);
+
+//Задание9
+
+const user = {
+
+    name: 'Aleksey',
+    age: 20,
+    isAdmin: true,
+}
+
+//Задание10
+
+let userName = prompt('Твоё имя');
+alert(`Привет ${userName}!`);
