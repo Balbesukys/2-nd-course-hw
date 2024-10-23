5da6e7f7a179d7a2cea69c6573dce0fffe924c7b
Balbesukys <lekha.arkhipov414@gmail.com>
2024-10-23 15:20:54 +0300

    Dz3

--- script.js
+++ script.js
@@ -134,3 +134,52 @@ switch (monthNumber) {
         alert('Не верно')
         break;
 }
+
+//Блок #3
+for (let i = 0; i < 2; i++) {
+    console.log('Привет');
+}
+
+//Задание2
+
+for (let i = 1; i < 5; i++) {
+    console.log(i);
+
+}
+
+//Задание3
+for (let i = 7; i <= 22; i++) {
+    console.log(i);
+}
+
+//Задание4
+
+const obj = {
+    Коля: '200',
+    Вася: '300',
+    Петя: '400'
+}
+for (const key in obj) {
+    console.log(`${key} - ${obj[key]} доларов`);
+}
+
+//Задание5 
+let n = 1000;
+let = num = 0;
+while (n >= 50) {
+    n /= 2;
+    console.log(n);
+    num++;
+}
+console.log(`Колличество циклов: ${num}`);
+
+//Задание6
+
+
+let dayFriday = 3;
+let allDay = 31;
+
+for (let o = dayFriday; o <= allDay; o += 7) {
+    console.log(`Сегодня пятница, ${o}-е число. Необходимо подготовить отчёт.`);
+
+}
