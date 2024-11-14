34d1afad4b3ddd0a28bd02705fc474609db9695b
Balbesukys <lekha.arkhipov414@gmail.com>
2024-11-14 20:29:07 +0300

    2 дз сделал

--- script.js
+++ script.js
@@ -225,7 +225,7 @@ function age() {
     let age = +prompt('Сколько тебе лет?');
 
     if (age < 0) {
-        alert('Вы ввели непрельное значение');
+        alert('Вы ввели неправельное значение');
 
     } else if (age >= 0 && age <= 12) {
         alert('Привет мой друг!');
@@ -281,3 +281,147 @@ const circle2 = {
 }
 console.log(circle1.methodGetArea());
 console.log(circle2.methodGetArea());
+
+
+//Массивы 
+
+//ЗАдание 1
+
+const arr = [1, 5, 4, 10, 0, 3]
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    if (arr[i] === 10) {
+        break;
+    }
+}
+//Задание 2
+const ard = [1, 5, 4, 10, 0, 3]
+console.log(ard.indexOf(4));
+
+//Задание 3
+const arf = [1, 3, 5, 10, 20]
+console.log(arf.join(' '));
+//Задание 4 
+
+const are = [];
+
+for (let i = 0; i < 3; i++) {
+    const are2 = [];
+    for (let i = 0; i < 3; i++) {
+        are2.push(1);
+    }
+    are.push(are2)
+}
+
+console.log(are);
+
+//Задание 5 
+const arre = [1, 1, 1];
+arre.push(2, 2, 2);
+console.log(arre);
+
+//Задание 6
+const arrd = [9, 8, 7, 'a', 6, 5]
+arrd.sort();
+console.log(arrd);
+//Задание 7
+const arrz = [9, 8, 7, 6, 5];
+
+const userNumber = +prompt('Введите число');
+if (arrz.includes(userNumber)) {
+    alert('Угадал');
+} else {
+    alert('Не угадал');
+}
+
+//Задание 8
+
+let str = 'abcdef';
+str = str.split('');
+console.log(str);
+str.reverse();
+str = str.join('');
+console.log(str);
+
+//Задание 9
+const arrt = [[1, 2, 3], [4, 5, 6]];
+const newArrt = [];
+
+for (let i = 0; i < arrt.length; i++) {
+    newArrt.push(...arrt[i]);
+}
+console.log(newArrt);
+
+//Задание 10
+const arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < arry.length; i++) {
+    if (arry[i + 1]) {
+        console.log(arry[i] + arry[i + 1]
+        );
+    }
+}
+
+//Задание 11 
+
+function square1(arr) {
+    return arr.map(item => item ** 2);
+}
+
+console.log(square1([1, 2, 3]));
+
+//Задние 12
+
+function getLength(arr) {
+    return arr.map(item => item.length);
+
+}
+console.log(getLength(['hi', 'world']));
+
+//Задание 13
+
+function negativeNumbers(array) {
+    return array.filter(item => item < 0);
+}
+console.log(negativeNumbers([1, 2, 3, -4, -5, -6, 7, 8, 9, 10]));
+
+//Задание 14
+
+function randomNumb() {
+    return Math.floor(Math.random() * 10);
+}
+
+const arrw = [];
+
+for (let i = 0; i < 10; i++) {
+    arrw.push(randomNumb());
+}
+
+console.log(arrw);
+
+const evenArr = [];
+
+for (let i = 0; i < arrw.length; i++) {
+    if (arrw[i] % 2 === 0) {
+        evenArr.push(arrw[i]);
+    }
+}
+
+console.log(evenArr);
+
+
+//Задание 15 
+
+function randomNumbers() {
+    return Math.floor(Math.random() * 10);
+}
+
+const arro = [];
+
+for (let i = 0; i < 3; i++) {
+    arro.push(randomNumbers());
+}
+
+
+console.log(arro);
+console.log(arro.reduce((a, b) => a + b / arro.length));
